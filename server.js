f68da2c1be9be1d8f968113ffd39b34910f1c888
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2022-01-22 15:06:49 -0500

    corrected stylesheet, updated packages

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>
 
 // GET Route for Notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
