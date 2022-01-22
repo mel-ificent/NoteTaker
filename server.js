8c10794027d578de0dc0b77552301a65647f6000
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2022-01-22 18:31:38 -0500

    revised port again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
